b8cc673212016ed226de2246b2e89bee227adfb6
Sandeep Shekhar Prasad <sandeep.prasad@jabong.com>
2014-07-28 05:49:16 +0530

    Getting and Cleaning Data Course Project

--- GettingAndCleaningDataCourseProject/README.md
+++ GettingAndCleaningDataCourseProject/README.md
@@ -4,5 +4,17 @@ Activity Recognition data set. The full description and the data can be found
 
 See `CodeBook.md` for details.
 
+How to run this program in R console:
+1. source('~/datasciencecoursera/GettingAndCleaningDataCourseProject/run_analysis.R'); //Please modify this as per your folder structure.
+2. cleanData();
 
- 
+Logic:
+1. Download the zipped file from [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+2. Download, save and unzip the downloaded zip file in data folder. The folder is created if it is not present
+3. Merge training and test datasets
+4. Extract only the measurements of the mean and standard deviation for each measurement
+5. Use descriptive activity names to name the activities in the dataset
+6. Use descriptive column name for subjects
+7. Combine mean-std values (x), activities (y) and subjects into one data frame
+8. Given X values, y values and subjects, create an independent tidy dataset with the average of each variable for each activity and each subject. This is done using "ddply" function in "plyr" library.
+9. Write tidy dataset as csv. The output file name is "UCI_HAR_tidy.csv".
--- GettingAndCleaningDataCourseProject/run_analysis.R
+++ GettingAndCleaningDataCourseProject/run_analysis.R
@@ -1,11 +1,13 @@
-# This R script gets and performs some cleaning on human activity data, built
-# from recordings of subjects performing daily activities while carrying
-# smartphone. The full description of the data set is available at:
+# This R script gets and performs some cleaning on Human Activity Recognition database 
+# built from the recordings of 30 subjects performing activities of daily living (ADL) 
+# while carrying a waist-mounted smartphone with embedded inertial sensors.
+# The full description of the data set is available at:
 # http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
 library(plyr)
 
-download.data = function() {
+##Download the data from source. Unzip it in data folder
+downloadData = function() {
     "Checks for data directory and creates one if it doesn't exist"
     if (!file.exists("data")) {
         message("Creating data directory")
@@ -21,46 +23,46 @@ download.data = function() {
     }
 }
 
-merge.datasets = function() {
+## Merge training and test datasets
+mergeDatasets = function() {
     "Merge training and test datasets"
     # Read data
     message("reading X_train.txt")
-    training.x <- read.table("data/UCI HAR Dataset/train/X_train.txt")
+    trainingX <- read.table("data/UCI HAR Dataset/train/X_train.txt")
     message("reading y_train.txt")
-    training.y <- read.table("data/UCI HAR Dataset/train/y_train.txt")
+    trainingY <- read.table("data/UCI HAR Dataset/train/y_train.txt")
     message("reading subject_train.txt")
-    training.subject <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
+    trainingSubject <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
     message("reading X_test.txt")
-    test.x <- read.table("data/UCI HAR Dataset/test/X_test.txt")
+    testX <- read.table("data/UCI HAR Dataset/test/X_test.txt")
     message("reading y_test.txt")
-    test.y <- read.table("data/UCI HAR Dataset/test/y_test.txt")
+    testY <- read.table("data/UCI HAR Dataset/test/y_test.txt")
     message("reading subject_test.txt")
-    test.subject <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
+    testSubject <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
     # Merge
-    merged.x <- rbind(training.x, test.x)
-    merged.y <- rbind(training.y, test.y)
-    merged.subject <- rbind(training.subject, test.subject)
+    mergedX <- rbind(trainingX, testX)
+    mergedY <- rbind(trainingY, testY)
+    mergedSubject <- rbind(trainingSubject, testSubject)
     # merge train and test datasets and return
-    list(x=merged.x, y=merged.y, subject=merged.subject)
+    list(x=mergedX, y=mergedY, subject=mergedSubject)
 }
 
-extract.mean.and.std = function(df) {
-    # Given the dataset (x values), extract only the measurements on the mean
-    # and standard deviation for each measurement.
-
+## Given the dataset (x values), extract only the measurements on the mean
+## and standard deviation for each measurement.
+extractMeanAndStd = function(df) {
     # Read the feature list file
     features <- read.table("data/UCI HAR Dataset/features.txt")
     # Find the mean and std columns
-    mean.col <- sapply(features[,2], function(x) grepl("mean()", x, fixed=T))
-    std.col <- sapply(features[,2], function(x) grepl("std()", x, fixed=T))
+    meanCol <- sapply(features[,2], function(x) grepl("mean()", x, fixed=T))
+    stdCol <- sapply(features[,2], function(x) grepl("std()", x, fixed=T))
     # Extract them from the data
-    edf <- df[, (mean.col | std.col)]
-    colnames(edf) <- features[(mean.col | std.col), 2]
+    edf <- df[, (meanCol | stdCol)]
+    colnames(edf) <- features[(meanCol | stdCol), 2]
     edf
 }
 
-name.activities = function(df) {
-    # Use descriptive activity names to name the activities in the dataset
+## Use descriptive activity names to name the activities in the dataset
+nameActivities = function(df) {
     colnames(df) <- "activity"
     df$activity[df$activity == 1] = "WALKING"
     df$activity[df$activity == 2] = "WALKING_UPSTAIRS"
@@ -71,36 +73,26 @@ name.activities = function(df) {
     df
 }
 
-bind.data <- function(x, y, subjects) {
-    # Combine mean-std values (x), activities (y) and subjects into one data
-    # frame.
-    cbind(x, y, subjects)
-}
-
-create.tidy.dataset = function(df) {
+##Function which needs to be called to create the tidy data
+cleanData = function() {
+    # Download data
+    downloadData()
+    # merge training and test datasets. merge.datasets function returns a list
+    # of three dataframes: X, y, and subject
+    merged <- mergeDatasets()
+    # Extract only the measurements of the mean and standard deviation for each
+    # measurement
+    cx <- extractMeanAndStd(merged$x)
+    # Name activities
+    cy <- nameActivities(merged$y)
+    # Use descriptive column name for subjects
+    colnames(merged$subject) <- c("subject")
+    #Combine mean-std values (x), activities (y) and subjects into one data frame
+    combined <- cbind(x, y, subjects)
     # Given X values, y values and subjects, create an independent tidy dataset
     # with the average of each variable for each activity and each subject.
     tidy <- ddply(df, .(subject, activity), function(x) colMeans(x[,1:60]))
-    tidy
-}
-
-clean.data = function() {
-    # Download data
-    download.data()
-    # merge training and test datasets. merge.datasets function returns a list
-    # of three dataframes: X, y, and subject
-    merged <- merge.datasets()
-    # Extract only the measurements of the mean and standard deviation for each
-    # measurement
-    cx <- extract.mean.and.std(merged$x)
-    # Name activities
-    cy <- name.activities(merged$y)
-    # Use descriptive column name for subjects
-    colnames(merged$subject) <- c("subject")
-    # Combine data frames into one
-    combined <- bind.data(cx, cy, merged$subject)
-    # Create tidy dataset
-    tidy <- create.tidy.dataset(combined)
     # Write tidy dataset as csv
     write.csv(tidy, "UCI_HAR_tidy.csv", row.names=FALSE)
 }
+
